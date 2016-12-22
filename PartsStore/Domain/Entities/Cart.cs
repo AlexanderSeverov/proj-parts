eeaad418557dc4cd03b18f10a1aef98985c7558d
AlexanderSeverov <sasha-parol80q@mail.ru>
2016-12-22 20:36:10 +0700

    update cart

---
+++ PartsStore/Domain/Entities/Cart.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public IEnumerable<CartLine> Lines { get { return lineCollection; } }
+
+        public void AddItem(Part part, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(b => b.Part.PartId == part.PartId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine { Part = part, Quantity = quantity });
+
+            }
+            else
+            {
+                line.Quantity += quantity;
+
+            }
+
+        }
+
+            public void RemoveLine(Part part)
+        {
+            lineCollection.RemoveAll(l => l.Part.PartId == part.PartId);
+        }
+
+        public decimal ComputerTotalValue()
+        {
+            return lineCollection.Sum(e => e.Part.Price * e.Quantity);
+         }
+
+        public void Clear()
+        {
+
+            lineCollection.Clear();
+        }
+
+        
+    }
+    
+    public class CartLine
+    {
+        public Part Part { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ PartsStore/UnitTest/CartTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests
+{
+
+    [TestClass]
+    public class CartTests
+    {
+        [TestMethod]
+        public void Can_Add_New_Lines()
+        {
+            Part part1 = new Part { PartId = 1, Name = "Part1" };
+            Part part2 = new Part { PartId = 2, Name = "Part2" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(part1, 1);
+            cart.AddItem(part2, 1);
+            List<CartLine> results = cart.Lines.ToList();
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Part, part1);
+            Assert.AreEqual(results[1].Part, part2);
+
+
+
+        }
+
+        [TestMethod]
+        public void Can_Add_Quantity_For_Existing_Lines()
+        {
+            Part part1 = new Part { PartId = 1, Name = "Part1" };
+            Part part2 = new Part { PartId = 2, Name = "Part2" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(part1, 1);
+            cart.AddItem(part2, 1);
+            cart.AddItem(part1, 5);
+            List<CartLine> results = cart.Lines.OrderBy(c=> c.Part.PartId).ToList();
+
+            Assert.AreEqual(results.Count(), 2);
+            Assert.AreEqual(results[0].Quantity, 6);
+            Assert.AreEqual(results[1].Quantity, 1);
+
+        }
+
+        [TestMethod]
+        public void Can_Remove_Line()
+        {
+            Part part1 = new Part { PartId = 1, Name = "Part1" };
+            Part part2 = new Part { PartId = 2, Name = "Part2" };
+            Part part3 = new Part { PartId = 3, Name = "Part3" };
+
+            Cart cart = new Cart();
+
+            cart.AddItem(part1, 1);
+            cart.AddItem(part2, 1);
+            cart.AddItem(part1, 5);
+            cart.AddItem(part3, 2);
+            cart.RemoveLine(part2);
+
+
+            Assert.AreEqual(cart.Lines.Where(c=> c.Part == part2).Count(),0);
+            Assert.AreEqual(cart.Lines.Count(), 2);
+
+
+        }
+
+        [TestMethod]
+        public void Calculate_Cart_Total()
+        {
+            Part part1 = new Part { PartId = 1, Name = "Part1",Price = 100 };
+            Part part2 = new Part { PartId = 2, Name = "Part2",Price = 55 };
+           
+
+            Cart cart = new Cart();
+
+            cart.AddItem(part1, 1);
+            cart.AddItem(part2, 1);
+            cart.AddItem(part1, 5);
+            decimal result = cart.ComputerTotalValue();
+
+            Assert.AreEqual(result,655);
+         }
+
+        [TestMethod]
+        public void Can_Clear_Contents()
+        {
+            Part part1 = new Part { PartId = 1, Name = "Part1", Price = 100 };
+            Part part2 = new Part { PartId = 2, Name = "Part2", Price = 55 };
+
+
+            Cart cart = new Cart();
+
+            cart.AddItem(part1, 1);
+            cart.AddItem(part2, 1);
+            cart.AddItem(part1, 5);
+            cart.Clear();
+
+            Assert.AreEqual(cart.Lines.Count(), 0);
+
+        }
+
+    }
+}
+
+
+
+
+
+
