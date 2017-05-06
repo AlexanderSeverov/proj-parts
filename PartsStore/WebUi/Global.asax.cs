847b960a4d22058cb890aa1bde19d143067fdb53
AlexanderSeverov <sasha-parol80q@mail.ru>
2017-05-06 09:28:43 +0700

    add web api
    new

--- PartsStore/WebUi/Global.asax.cs
+++ PartsStore/WebUi/Global.asax.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WebUi.App_Start;
 using WebUi.Infrastructure;
 
 namespace WebUi
@@ -13,6 +15,7 @@ namespace WebUi
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
